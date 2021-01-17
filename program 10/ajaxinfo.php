0dc187acfebbf99d65f3110ae1d4effccb0448a8
mohammedburhaanali <70208057+mohammedburhaanali@users.noreply.github.com>
2021-01-17 15:44:38 +0530

    Update ajaxinfo.php

--- program 10/ajaxinfo.php
+++ program 10/ajaxinfo.php
@@ -1,3 +1,71 @@
+<!DOCTYPE html> <html>
+ <head>
+ <title>Selection Sort</title> </head>
+ <!-- jQuery 3 -->
+ <script src="jquery.min.js"></script>
+ <script type="text/javascript">
+ var globalData = null;
+ var tempData;
+ if (tempData === null || tempData === undefined) {
+ tempData = {};
+ }
+ tempData = {
+ saveRecord: function ()
+ {
+ var url = "ajaxInfo.php";
+ }
+ var formEQData = new FormData($('#formRecord')[0]);
+ formEQData.append("saveRecord", "saveRecord");
+ $.ajax({type: "POST", url: url, async: false, dataType: 'json', cache: false, processData: false, 
+   contentType: false, data: formEQData,
+ success: function (obj) {
+ alert(obj.msg);
+ tempData.getRecord(); // Calling function to fetch the Record from DB.
+ }
+ });
+ },
+ getRecord: function () {
+ var url = "ajaxInfo.php";
+ var myData = {getRecord: 'getRecord'};
+ $.ajax({type: "POST", url: url, async: false, dataType: 'json', data: myData, success: function
+(obj)
+ {
+ globalData = obj.studentArr; // Assigning to Global Variable
+ var content = "";
+ $('#tableRow').html("");
+ for (var i = 0; i < obj.studentArr.length; i++)
+ {
+ content += '<tr><td>' + obj.studentArr[i].stu_id + '</td><td>' +
+obj.studentArr[i].stu_name + '</td>'
+ + '<td>' + obj.studentArr[i].stu_mobile + '</td><td>' + obj.studentArr[i].stu_email
++ '</td></tr>';
+ }
+ $('#tableRow').append(content);
+ }
+ });
+ },
+ selectionSort: function () {
+ var url = "ajaxInfo.php";
+ var myData = {selectionSort: 'selectionSort'};
+ $.ajax({type: "POST", url: url, async: false,
+ dataType: 'json', data: myData,
+ success: function (obj)
+ {
+ var content = "";
+ $('#tableRow').html("");
+ for (var i = 0; i < obj.sortedArr.length; i++)
+ {
+ for (var j = 0; j < globalData.length; j++)
+ {
+ if (obj.sortedArr[i] == globalData[j].stu_id)
+ {
+ content += '<tr><td>' + globalData[j].stu_id + '</td><td>' +
+globalData[j].stu_name + '</td>'
+ + '<td>' + globalData[j].stu_mobile + '</td><td>' + globalData[j].stu_email +
+'</td></tr>';
+ }
+ }
+ }
  $('#tableRow').append(content);
  }
  });
